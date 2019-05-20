af5defdc1aca9861b3b0a1d46aee03893994a49f
João Santana <joaovictorsantt@gmail.com>
2019-05-20 18:56:32 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <h1>HTTP com Axios</h1>
+    <h1>Cadastro de Usuários</h1>
     <b-card>
       <b-form-group label="Nome:">
         <b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome"></b-form-input>
@@ -9,15 +9,35 @@
         <b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe o E-mail"></b-form-input>
       </b-form-group>
       <hr>
+
       <b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+      <b-button @click="obterUsuarios" size="lg" variant="success" class="ml-2">Obter Usuários</b-button>
     </b-card>
+    <hr>
+
+    <b-list-group>
+      <b-list-group-item v-for="(usuario,id) in usuarios" :key="id">
+        <strong>Nome:</strong>
+        {{usuario.nome}}
+        <br>
+        <strong>E-mail:</strong>
+        {{usuario.email}}
+        <br>
+        <strong>ID</strong>
+        {{id}}
+        <br>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
+      usuarios: [],
       usuario: {
         nome: "",
         email: ""
@@ -27,8 +47,14 @@ export default {
 
   methods: {
     salvar() {
-      this.$http.post("usuarios.json", this.usuario).then(resp => {
+      this.$http.post("users", this.usuario).then(resp => {
         (this.usuario.nome = ""), (this.usuario.email = "");
+      });
+    },
+
+    obterUsuarios() {
+      axios("http://frontendapi.cm2tech.com.br/users").then(res => {
+        this.usuarios = res.data;
       });
     }
   }
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://frontendapi.cm2tech.com.br";
+//axios.defaults.baseURL = "http://frontendapi.cm2tech.com.br";
 
 Vue.use({
   install(Vue) {
-    Vue.prototype.$http = axios;
+    Vue.prototype.$http = axios.create({
+      baseURL: "http://frontendapi.cm2tech.com.br/users"
+    });
   }
 });
